fc595da3d0c6866284422b91d62c56f8c3b591f1
Paco <pakokrew@gmail.com>
2017-01-08 20:37:19 +0100

    Footer

--- app/js/components/layout/Layout.js
+++ app/js/components/layout/Layout.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Container } from 'semantic-ui-react'
-import { Menu } from 'semantic-ui-react'
+import { Container, Menu, Button, Icon } from 'semantic-ui-react'
 
 const Layout = ({ children }) =>
   <div>
@@ -14,6 +13,20 @@ const Layout = ({ children }) =>
     <Container text className="main">
       {children}
     </Container>
+    <Menu attached="bottom" inverted>
+      <Container>
+        <Menu.Item>
+          Made with &nbsp; <Icon name="heart" /> by &nbsp;<a href="https://ngfar.io" target="_blank">ngfar</a>
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Button
+            icon="github"
+            compact
+            onClick={() => window.open("https://github.com/pakokrew/stellar-portal")}
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
   </div>;
 
 Layout.propTypes = {
