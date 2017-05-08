e635ef42bd7bd36691cbd53b0cdc69805887bdc1
Maximilian Stoll <max@quantoz.com>
2017-05-08 21:26:24 +0200

    added registration functionality

--- app/js/actions-creators/account.js
+++ app/js/actions-creators/account.js
@@ -6,8 +6,12 @@ import { ASYNC_FETCH_ACCOUNT, ASYNC_CREATE_TEST_ACCOUNT } from '../constants/asy
 
 import { getNetwork } from '../selectors/stellarData';
 
+import {federationRegister,federationCreate, federationUrl} from '../federation/index'
+
 const { getAccount, switchNetwork: switchNetworkInstance, generateTestPair } = StellarServer;
 const { KeypairInstance } = StellarTools;
+
+
 
 export const setAccount = keys => (dispatch, getState) => {
   dispatch(AsyncActions.startFetch(ASYNC_FETCH_ACCOUNT));
@@ -38,12 +42,34 @@ export const switchNetwork = network => (dispatch, getState) => {
   dispatch(AccountActions.switchNetwork(network));
 };
 
-export const createTestAccount = () => (dispatch) => {
-  dispatch(AsyncActions.startLoading(ASYNC_CREATE_TEST_ACCOUNT));
+export const createTestAccount = (e, {formData}) => (dispatch) => {
+  e.preventDefault()
+  dispatch(AsyncActions.startLoading(ASYNC_CREATE_TEST_ACCOUNT)); 
+
+  const address = {
+    street: formData["street"],
+    nr: formData["address_nr"],
+    city: formData["city"],
+    postal_code: formData["postal_code"],
+    country: formData["country"]
+  }
+
+  const account = {
+    stellar_address : formData["user_name"],
+    passport_nr : formData["passport_nr"],
+    address : address,
+    first_name : formData["first_name"],
+    last_name : formData["last_name"]
+  }
+
   generateTestPair()
     .then((newPair) => {
+      console.log("Hello")
       dispatch(AsyncActions.stopLoading(ASYNC_CREATE_TEST_ACCOUNT));
       dispatch(setAccount(newPair));
+      federationCreate({stellar_address : account.stellar_address, password: formData["password"]}, newPair).then(() => {
+          federationRegister(account, newPair)
+      }).catch(console.error);
     })
     .catch(console.error);
 };
