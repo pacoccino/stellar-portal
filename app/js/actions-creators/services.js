e10ae810bf1106032e53497a6689dd8bff2f1b35
Paco <pakokrew@gmail.com>
2017-05-09 11:56:56 +0200

    url redirect

--- app/js/actions-creators/services.js
+++ app/js/actions-creators/services.js
@@ -1,8 +1,3 @@
 import config from 'js/config';
-// import config from 'js/config';
-
-const config = {
-  FEDERATION_DOMAIN: 'dex-backend.herokuapp.com',
-};
 
 export const getStellarAddress = username => `${username}*${config.FEDERATION_DOMAIN}`;
