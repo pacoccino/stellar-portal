d9ba1ae0a4985fc5a8952d3ca0463df13d16a7f3
Paco <pakokrew@gmail.com>
2017-05-09 12:55:19 +0200

    front page

--- app/js/components/views/WelcomeView.js
+++ app/js/components/views/WelcomeView.js
@@ -1,53 +1,74 @@
 import React, { Component } from 'react';
 import { Container, Header, List } from 'semantic-ui-react';
 
-import AccountSelector from '../../elements/StellarContainers/AccountSelector';
-
-import logo from '../../../../content/assets/images/stellarportal.png';
+import dexLogo from '../../../../content/assets/images/dex-logo.png';
+import secureImg from '../../../../content/assets/images/SECURE.png';
+import privateImg from '../../../../content/assets/images/PRIVATE.png';
+import decentralizedImg from '../../../../content/assets/images/DECENTRALIZED.png';
 
 const styles = {
   logo: {
-    height: '16rem',
+    height: '8rem',
+    marginTop: '50px',
+    marginBottom: '40px',
   },
   title: {
-    fontSize: '3.5rem',
-    color: 'white',
-    fontWeight: 400,
-    paddingTop: '0.5rem',
-    paddingBottom: '2.5rem',
-  },
-  paragraph: {
-    fontSize: '1.3rem',
-    padding: '0.6rem',
-    color: 'white',
-  },
-  paragraphE: {
-    fontSize: '1.4rem',
-    padding: '0.6rem',
-    color: 'white',
-  },
-  link: {
-    color: '#50a4f5',
-    fontWeight: 600,
-  },
-  description: {
-    paddingTop: '6rem',
-    paddingBottom: '6rem',
-  },
-  subtitle: {
-    fontSize: '2.5rem',
-    color: 'white',
-    fontWeight: 400,
-    paddingTop: '0.5rem',
-    paddingBottom: '0.5rem',
-  },
-  screen: {
-    height: '32rem',
-  },
-  featuresItem: {
-    fontSize: '1.3rem',
+    fontSize: '2.2rem',
     color: 'white',
     fontWeight: 300,
+    paddingTop: '0.5rem',
+    paddingBottom: '2.5rem',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: '1.4rem',
+    maxWidth: '800px',
+    color: 'white',
+    fontWeight: 300,
+    paddingTop: '0.5rem',
+    paddingBottom: '2.5rem',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  three: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginTop: '30px',
+  },
+  one: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '230px',
+    textAlign: 'center',
+    marginLeft: '30px',
+    marginRight: '30px',
+  },
+  oneTitle: {
+    fontSize: '1.3rem',
+    color: 'white',
+    fontWeight: 800,
+    paddingTop: '2rem',
+    paddingBottom: '1.5rem',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  oneText: {
+    fontSize: '1rem',
+    color: 'white',
+    fontWeight: 400,
+    paddingTop: '0.5rem',
+    paddingBottom: '1.3rem',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  oneImage: {
+    width: '125px',
   },
 };
 
@@ -55,39 +76,54 @@ class WelcomeScreen extends Component {
   render() {
     return (
       <div className="welcome-container">
-        <div className="welcome-container-overlay" />
-        <Container textAlign="center">
-          <img src={logo} style={styles.logo} className="welcome-logo" alt="welcome" />
-          <Header style={styles.title}>
-            Stellar Portal
-          </Header>
-          <p style={styles.paragraph}>
-            Stellar Portal is a web app designed to access the
-            &nbsp;<a style={styles.link} href="https://www.stellar.org/">Stellar Network</a>.
-            <br />
-            It allows you to consult account informations such as balances and offers,
-            see orderbook and to make transactions.
-            <br />
-            This application relies <b>exclusively</b> on <a style={styles.link} href="https://www.stellar.org/developers/reference">Horizon API</a>.
-          </p>
-          <p style={styles.paragraphE}>
-            Give it a try by entering an account ID or a Seed to see it in action:
-          </p>
-          <div style={styles.description}>
-            <Header style={styles.subtitle}>
-              Features
-            </Header>
-            <List bulleted>
-              <List.Item style={styles.featuresItem}>Balances view and edit trustlines</List.Item>
-              <List.Item style={styles.featuresItem}>Send payments</List.Item>
-              <List.Item style={styles.featuresItem}>Issue assets</List.Item>
-              <List.Item style={styles.featuresItem}>Create/Merge accounts</List.Item>
-              <List.Item style={styles.featuresItem}>Account offers view and edit</List.Item>
-              <List.Item style={styles.featuresItem}>List of payments</List.Item>
-              <List.Item style={styles.featuresItem}>Watch orderbooks</List.Item>
-            </List>
+        <img src={dexLogo} style={styles.logo} className="welcome-logo" alt="welcome" />
+        <p style={styles.title}>
+          Decentralized Exchange
+        </p>
+        <p style={styles.subtitle}>
+          Dex is a p2p exchange designed for the Stellar Network. <br/>
+          It allows you to buy and sell verified assets
+        </p>
+        <div style={styles.three}>
+          <div style={styles.one}>
+            <img src={secureImg} style={styles.oneImage} alt="welcome" />
+            <div style={styles.oneTitle}>
+              SECURE
+            </div>
+            <div style={styles.oneText}>
+              Entire exchange continually audited and inspected for unusual activity
+            </div>
+            <div style={styles.oneText}>
+              Two-factor authentication available for your protection
+            </div>
           </div>
-        </Container>
+
+          <div style={styles.one}>
+            <img src={decentralizedImg} style={styles.oneImage} alt="welcome" />
+            <div style={styles.oneTitle}>
+              decentralized
+            </div>
+            <div style={styles.oneText}>
+              ENtire exchange continually
+              audited and inspected
+              for unusual activity
+            </div>
+            <div style={styles.oneText}>
+              Two-factor authentication
+              available for your protection
+            </div>
+          </div>
+
+          <div style={styles.one}>
+            <img src={privateImg} style={styles.oneImage} alt="welcome" />
+            <div style={styles.oneTitle}>
+              private
+            </div>
+            <div style={styles.oneText}>
+              we concerned about your privacy for this reason all your data will be protected with high level of encription
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
