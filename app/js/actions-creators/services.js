f1f34e1714fd685cca48865e6fd65aeabb360756
Maximilian Stoll <max@quantoz.com>
2017-05-09 11:45:57 +0200

    updated user registration

--- app/js/actions-creators/services.js
+++ app/js/actions-creators/services.js
@@ -1,3 +1,8 @@
 import config from 'js/config';
+// import config from 'js/config';
+
+const config = {
+  FEDERATION_DOMAIN: 'dex-backend.herokuapp.com',
+};
 
 export const getStellarAddress = username => `${username}*${config.FEDERATION_DOMAIN}`;
